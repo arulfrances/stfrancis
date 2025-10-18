d4bd2ee2226097b3609550e162234ff70eeac5c1
Arul Frances <arulfrances@gmail.com>
2025-10-18 11:59:26 +0530

    Added Index

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,15 @@
-import { Box, Heading, Text, Image } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import Layout from "../components/Layout";
+import HeroSlider from "../components/Slider";
 
 export default function Home() {
   return (
     <Layout>
+      <HeroSlider />
       <Box textAlign="center" mb={8}>
-        <Image
-          src="/images/church.jpg"
-          alt="St Francis of Assisi Church"
-          borderRadius="md"
-          mx="auto"
-          maxH="400px"
-          objectFit="cover"
-        />
-        <Heading mt={4}>Welcome to St. Francis of Assisi Church</Heading>
+        <Heading>Welcome to St. Francis of Assisi Church</Heading>
         <Text mt={4} maxW="600px" mx="auto" fontSize="lg" color="gray.700">
-          Founded in 1990, our church in Assisi Nagar, Ganapathy, Coimbatore is
-          a vibrant faith community devoted to worship, outreach, and service.
+          Founded in 1990, our church in Assisi Nagar, Ganapathy, Coimbatore is a vibrant faith community devoted to worship, outreach, and service.
         </Text>
       </Box>
     </Layout>
